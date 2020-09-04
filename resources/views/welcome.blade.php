b9864e62060f7e73118673311281c2cc96a9826e
zoranzlatko <zoranzlatko@gmail.com>
2020-09-04 14:27:11 +0200

    zoran test commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+<p>kdsahfksajdhfskljadhfs,jdhvfs,jdhvs,jdhvs,jdhv,sjdhv,jsdhv,jsdhv,sjdhvs,jd</p>
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
